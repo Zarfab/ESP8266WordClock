f6fdd65c961a2d31e52a0570a35bc8c459c9775c
unknown <fabien.grisard@gmail.com>
2019-09-27 13:25:07 +0200

    add OTA

--- ESP8266WordClock.ino
+++ ESP8266WordClock.ino
@@ -1,7 +1,7 @@
 #include <ESP8266WiFi.h>
-#include <ESP8266LLMNR.h>
 #include <ESP8266WebServer.h>
 #include <WiFiManager.h>         //https://github.com/tzapu/WiFiManager
+#include <ArduinoOTA.h>
 #include <ArduinoJson.h>
 #define FASTLED_INTERNAL // Disable version number message in FastLED library (looks like an error)
 #include <FastLED.h>
@@ -336,11 +336,41 @@ void setup() {
   WiFiManager wifiManager;
   wifiManager.autoConnect("WordClockAP");
 
-  LLMNR.begin("wordclock");
-  #ifdef SERIAL_DEBUG
-  SERIAL_DEBUG.println(F("LLMNR responder started, go to wordclock.local to access it"));
-  #endif
-  
+  ArduinoOTA.onStart([]() {
+    String type;
+    if (ArduinoOTA.getCommand() == U_FLASH) {
+      type = "sketch";
+    } else { // U_SPIFFS
+      type = "filesystem";
+    }
+    SPIFFS.end();
+    Serial.println("Start updating " + type);
+  });
+  ArduinoOTA.onEnd([]() {
+    Serial.println("\nEnd");
+    delay(1000);
+    ESP.restart();
+  });
+  ArduinoOTA.onProgress([](unsigned int progress, unsigned int total) {
+    Serial.printf("Progress: %u%%\r", (progress / (total / 100)));
+  });
+  ArduinoOTA.onError([](ota_error_t error) {
+    Serial.printf("Error[%u]: ", error);
+    if (error == OTA_AUTH_ERROR) {
+      Serial.println("Auth Failed");
+    } else if (error == OTA_BEGIN_ERROR) {
+      Serial.println("Begin Failed");
+    } else if (error == OTA_CONNECT_ERROR) {
+      Serial.println("Connect Failed");
+    } else if (error == OTA_RECEIVE_ERROR) {
+      Serial.println("Receive Failed");
+    } else if (error == OTA_END_ERROR) {
+      Serial.println("End Failed");
+    }
+    delay(1000);
+    ESP.restart();
+  });
+  ArduinoOTA.begin();
 
   server.on("/", HTTP_GET, handleRoot);
   server.on("/", HTTP_POST, handleConfig);
@@ -380,6 +410,7 @@ void setup() {
 
 
 void loop() {
+  ArduinoOTA.handle();
   server.handleClient();
 
   if(millis() - FastLedTimer >= FASTLED_REFRESH) {
